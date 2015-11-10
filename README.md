cfff5eda968246a085c580d4ae44889bbc42573e
Pete <pld9bc@mail.missouri.edu>
2015-11-10 12:28:13 -0600

    working on db integration

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # trvBud
+
+to import db into mongo...
+
+$ iconv -f ISO-8859-15 -t UTF-8 wc.txt > wc.txt
+
+$ mongoimport --db world --collection city --file ./wc.txt --type csv --headerline
--- node/index.js
+++ node/index.js
@@ -1,16 +1,39 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+/*
+ *  set view engine to jade
+ **/
 app.set('view engine', 'jade');
 
+/*
+ *  set static assets
+ **/
 app.use('/js', express.static(__dirname + '/views/js'));
 app.use('/css', express.static(__dirname + '/views/css'));
 
+/*
+ *  mongo db implement
+ **/
+mongoose.connect('mongodb://localhost/world');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log("connected to mongo")
+});
+
+/*
+ *  routing
+ **/
 app.get('/', function(req, res){ 
     res.render('main.jade');
 });
 
+/*
+ *  main server sequence
+ **/
 var server = app.listen(8080,function(){
     console.log("We have started our server on port ...");
     if (app.get('env') === 'development') {
